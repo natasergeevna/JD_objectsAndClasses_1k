6e5f79436d1e87f9df821ba96d6e5dca2fa109bb
nataliashashina <sergeevnaa01@mail.ru>
2023-02-23 22:24:24 +0300

    Objects and classes v1

---
+++ src/Book.java
@@ -0,0 +1,37 @@
+public class Book {
+    //используемые поля
+    String nameBook;
+    Autor autorBook;
+    int yearBook;
+
+    //rонструктор
+    public Book(String nameBook, Autor autorBook, int yearBook) {
+        this.nameBook = nameBook;
+        this.autorBook = autorBook;
+        this.yearBook = yearBook;
+    }
+
+    //геттер
+    public String getNameBook() {
+        return this.nameBook;
+    }
+
+    public Autor getAutorBook() {
+        return this.autorBook;
+    }
+
+    public int getYearBook() {
+        return this.yearBook;
+    }
+
+    //сеттер
+    public void setYearBook(int yearBook) {
+        this.yearBook = yearBook;
+    }
+
+
+    //для красивой печати
+    public String toString() {
+        return "Книга: " + getNameBook() + " Автор: " + autorBook.toString() + " Год: " + getYearBook();
+    }
+}
